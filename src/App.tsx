f64f11c7e8acb45b55c9ca27815df5f87554d2a1
Артем Масловский <a.maslouski@NovaCom.by>
2025-05-02 17:59:11 +0300

    createTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Authorization from './pages/Authorization'
+import ParkingPlaces from './pages/ParkingPlaces';
 
 function App() {
   return (
     <div className="App">
-      <Authorization />
+      <ParkingPlaces />
     </div>
   );
 }
